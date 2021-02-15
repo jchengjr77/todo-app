b03180d3ede99990f2df5afaeeebead84b2fa830
Jonathan Cheng <jonathanchengjr77@gmail.com>
2021-02-15 17:51:23 +0800

    Finished basic react frontend for rec3

--- todo-app-backend/index.js
+++ todo-app-backend/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const port = 3000;
+const port = 3001;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
